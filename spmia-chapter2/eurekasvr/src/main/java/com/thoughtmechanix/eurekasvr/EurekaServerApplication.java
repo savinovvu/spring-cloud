fa7621a1aa59f37ea60e26f3d269fc6e94868f55
skorpion <savinov_vu@inbox.ru>
2018-05-31 17:47:25 +0800

    2. discover service: implement discovery organizations

--- spmia-chapter2/eurekasvr/src/main/java/com/thoughtmechanix/eurekasvr/EurekaServerApplication.java
+++ spmia-chapter2/eurekasvr/src/main/java/com/thoughtmechanix/eurekasvr/EurekaServerApplication.java
@@ -2,10 +2,12 @@ package com.thoughtmechanix.eurekasvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
--- spmia-chapter2/organization-service/src/test/java/com/thoughtmechanix/organizationservice/EagleEyeOrganizationServiceApplicationTests.java
+++ spmia-chapter2/organization-service/src/test/java/com/thoughtmechanix/organizationservice/EagleEyeOrganizationServiceApplicationTests.java
@@ -1,18 +1,27 @@
 package com.thoughtmechanix.organizationservice;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@RestController("organization")
 public class EagleEyeOrganizationServiceApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @GetMapping()
+    public String getAll(){
+        return "this is all organizations";
     }
 
 }
