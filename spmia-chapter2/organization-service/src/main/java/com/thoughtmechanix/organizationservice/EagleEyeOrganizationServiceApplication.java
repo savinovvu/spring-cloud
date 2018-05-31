82604bb1a50eb0a1a0a9a1f774bb88de46e0a06c
skorpion <savinov_vu@inbox.ru>
2018-05-31 16:37:58 +0800

    2. discover service: add eurika and organization services.

---
+++ spmia-chapter2/organization-service/src/main/java/com/thoughtmechanix/organizationservice/EagleEyeOrganizationServiceApplication.java
@@ -0,0 +1,14 @@
+package com.thoughtmechanix.organizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class EagleEyeOrganizationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EagleEyeOrganizationServiceApplication.class, args);
+    }
+}
